75e3c8b6240cddcada6c0bf2b8eeba23ca712546
Anshul Singhvi <anshulsinghvi@gmail.com>
2026-07-01 17:21:26 -0400

    OpenCRG: package skeleton, test harness, fixtures

---
+++ lib/OpenCRG/src/OpenCRG.jl
@@ -0,0 +1,10 @@
+module OpenCRG
+
+include("header.jl")
+include("payload.jl")
+include("read.jl")
+include("transform.jl")
+
+export read_crg, road_surface_grid, CRGData
+
+end # module OpenCRG
---
+++ lib/OpenCRG/test/runtests.jl
@@ -0,0 +1,13 @@
+# lib/OpenCRG/test/runtests.jl
+using Test
+using OpenCRG
+
+const DATA = joinpath(@__DIR__, "data")
+
+@testset "OpenCRG.jl" begin
+    include("test_header.jl")
+    include("test_payload.jl")
+    include("test_read.jl")
+    include("test_transform.jl")
+    include("test_crossvalidate.jl")
+end
